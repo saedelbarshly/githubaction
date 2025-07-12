651bef7214ac6572a13848f32d7c7e255799f71a
saedelbarshly1 <saedelbarshly@gmail.com>
2025-07-12 13:09:57 +0300

    new

--- src/test.sh
+++ src/test.sh
@@ -1,6 +1,7 @@
-EXPECTED="Hello, Test!"
+#!/bin/bash
 
-OUTPUT=$(node -e "console.log(require('.src/app')('Test!'))")
+EXPECTED="Hello, Test!"
+OUTPUT=$(node -e "console.log(require('./src/app')('Test!'))")
 
 if [ "$OUTPUT" == "$EXPECTED" ]; then
     echo "Test passed 👌"
