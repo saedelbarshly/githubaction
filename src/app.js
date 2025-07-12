8a14ead91e8e5410ef235a8af9632edff4baf587
Alsaeed Mohamed Albarshly <166986525+saedelbarshly@users.noreply.github.com>
2025-07-12 15:10:01 +0300

    Update app.js

--- src/app.js
+++ src/app.js
@@ -7,3 +7,8 @@ module.exports = greet;
 // if (require.main === module) {
 //   console.log(greet("World"));
 // }
+// curl -X POST \
+// > -H "Authorization: token " \
+// > -H "Accept: application/vnd.github.v3+json" \
+// > https://api.github.com/repos/saedelbarshly/githubaction/dispatches \
+// >  -d '{"event_type":"incident_report"}'
